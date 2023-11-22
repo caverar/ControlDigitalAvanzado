720b4155f673689445f18e16d6f3f62f08e77b1e
Camilo Andres Vera Ruiz <camilovera9811@gmail.com>
2023-11-22 03:17:47 -0500

    pi report finished

--- 4.pid_design/1.PI/data_loader.m
+++ 4.pid_design/1.PI/data_loader.m
@@ -1,7 +1,26 @@
 classdef data_loader
     methods(Static)
-        function out = square_signal_loader(file_name, min_value, max_value)
-            file = load(file_name)
+        function [out,ctr] = square_signal_loader(file_name, freq, ...
+                signal_period, min_value, max_value)
+            % load file
+            file = readtable(file_name);
+            ref = file.r;
+            omega = file.omega;
+            u = file.u;
+            % Order data
+            zero_index = 0;
+            i = 2;
+            while i<length(ref) && zero_index == 0
+                if ref(i-1)==min_value && ref(i)==max_value
+                    zero_index=i;
+                end
+                i=i+1;            
+            end
+            begin_index = zero_index-(freq*signal_period/2);
+            end_index = (zero_index-(freq*signal_period/2)-1) + ...
+                (freq*signal_period*2);
+            out=omega(begin_index: end_index +1);
+            ctr=u(begin_index: end_index +1);
         end 
     end
 end
